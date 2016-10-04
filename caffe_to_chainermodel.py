f576b2d20b30ccbdace2fe30c9b79e31c554e8e8
ta-oyama <oyamat4@gmail.com>
2016-10-04 13:28:38 +0900

    modify caffe_to_chainermodel.py

--- caffe_to_chainermodel.py
+++ caffe_to_chainermodel.py
@@ -5,11 +5,14 @@ import cPickle as pickle
 from chainer.functions.caffe import CaffeFunction
 import chainer.serializers as s
 
+import util
+
 model_dir = 'pretrained_models'
+util.check_dirs(model_dir)
 print("Reading caffe model...")
-func = CaffeFunction(osp.join(model_dir,"bvlc_alexnet.caffemodel") # read caffe model
+func = CaffeFunction(osp.join(model_dir,"bvlc_alexnet.caffemodel")) # read caffe model
 print("Writing as chainer model with hdf5...")
 s.save_hdf5(osp.join(model_dir,"alexnet.h5"), func) # write as chainer model
 print("Writing as chainer model with pickle...")
-pickle.dump(func, open(osp.join(model_dir,"alexnet.pkl", "wb"), -1)
+pickle.dump(func, open(osp.join(model_dir,"alexnet.pkl", "wb"), -1))
 print("Done")
